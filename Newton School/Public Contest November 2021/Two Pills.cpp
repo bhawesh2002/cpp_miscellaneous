4d8643d37a36762ff467406383b6800c659fab70
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-09 12:39:34 +0530

    add problem statement

---
+++ Newton School/Public Contest November 2021/Two Pills.cpp
@@ -0,0 +1,8 @@
+/*Problem Statement
+You are fighting against a monster.
+The monster has health M while you have initial health Y.
+To defeat him, you need health strictly greater than the monster's health.
+
+You have access to two types of pills, red and green. Eating a red pill adds R to your health while eating a green pill multiplies your health by G.
+
+Determine if it is possible to defeat the monster by eating at most one pill of any kind.*/
