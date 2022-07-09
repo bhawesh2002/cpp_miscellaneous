32fce7c6d2549df3057cc956b3f65358331070a3
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-09 13:03:58 +0530

    add the problem statement

---
+++ Newton School/Public Contest November 2021/simple split.cpp
@@ -0,0 +1,8 @@
+/*You are given an array of integers of length N, where N is even.
+
+You need to split the array into two parts of equal size in such a way that the difference between the sums of the parts is maximised. The sum of a part is defined as the sum of the elements it contains.
+
+Note that each element of the original array must be in exactly one of the parts.
+
+Print the maximum possible difference.*/
+// Link:- https://my.newtonschool.co/playground/code/1pctmsttzq3g/
