f7395a16ed39ba17561ff8f2986a6bc475b3361a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-09 12:40:04 +0530

    add link to the problem

--- Newton School/Public Contest November 2021/Two Pills.cpp
+++ Newton School/Public Contest November 2021/Two Pills.cpp
@@ -6,3 +6,4 @@ To defeat him, you need health strictly greater than the monster's health.
 You have access to two types of pills, red and green. Eating a red pill adds R to your health while eating a green pill multiplies your health by G.
 
 Determine if it is possible to defeat the monster by eating at most one pill of any kind.*/
+/*Link:- https://my.newtonschool.co/playground/code/1l4y80gbwawl/*/
