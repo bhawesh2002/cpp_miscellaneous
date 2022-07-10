e1bf22f0275cab9874f14f2a1338fc6973085fd8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-10 11:30:26 +0530

    add input and output part of the problem statement

--- Newton School/Public Contest November 2021/Black Ball White Ball.cpp
+++ Newton School/Public Contest November 2021/Black Ball White Ball.cpp
@@ -5,6 +5,11 @@ If it is a black ball, it is thrown away.
 If it is a white ball, it is put back inside the bag. 
 The player who draws the second black ball wins the game. 
 You are required to find the probability of Alice winning the game.*/
+/*Input
+The input consists of a single line containing two integers B (2 ≤ B ≤ 108) and W (0 ≤ W ≤ 108).*/
+/*Output
+The answer can always be represented as a fraction P⁄Q, where P is an integer and Q is a natural number not divisible by 109 + 7.
+Print P · Q-1 modulo 109 + 7.*/
 #include <iostream>
 
 using namespace std;
