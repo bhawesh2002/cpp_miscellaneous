b2beeb1652ace384a56a0124ac2786466af387ef
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-10 11:27:02 +0530

    basic structure of the program

---
+++ Newton School/Public Contest November 2021/Black Ball White Ball.cpp
@@ -0,0 +1,14 @@
+/*Alice has a bag containing B black balls and W white balls. Alice and bob decide to play a game.
+Alice and Bob make alternate moves. Alice makes the first move. 
+In each move, the player draws a ball out of the bag. 
+If it is a black ball, it is thrown away. 
+If it is a white ball, it is put back inside the bag. 
+The player who draws the second black ball wins the game. 
+You are required to find the probability of Alice winning the game.*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
